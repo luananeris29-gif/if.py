83ce266fb931770fca59c254780411160797f695
Luana Neris <luananeris29@gmail.com>
2026-08-27 22:01:55 -0300

    luana2.py

---
+++ luana2.py
@@ -0,0 +1,11 @@
+idade = 20
+
+print("--- Exemplo 2: if-else ---")
+print(f"Idade informada: {idade}")
+
+if idade >= 18:
+    print("Acesso permitido. Você é maior de idade.")
+else:
+    print("Acesso negado. Você é menor de idade.")
+
+       
