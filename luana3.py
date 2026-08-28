925850fddc15158f766db887da21ae3e50024609
Luana Neris <luananeris29@gmail.com>
2026-08-27 22:14:43 -0300

    luana3.py

---
+++ luana3.py
@@ -0,0 +1,14 @@
+nota = 59
+print ("---Exemplo 3: if-elif-else ---")
+print (f"nota do aluno: {nota}")
+
+if nota >= 90:
+      print("Conceito: A")
+elif nota >= 80: 
+      print("Conceito:B")
+elif nota >= 70:
+     print("Conceito: C")
+elif nota >= 60:
+      print("Conceito: D")
+else: 
+       print("conceito: F (Reprovado)")
